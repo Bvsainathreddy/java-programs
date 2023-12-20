bbcb48793e3a06572d1803f82c88e591b80c3ca1
Bvsainathreddy <130168768+Bvsainathreddy@users.noreply.github.com>
2023-12-20 14:43:08 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class pattern
+{
+    public static void main(String args[])
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("\n enter the number of patterns");
+        int n = reader.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+      
